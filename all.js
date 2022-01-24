9af49a6725a6b005ea42c37a088aec5c1ca92acc
redtail <hongwei0522@gmail.com>
2022-01-24 22:39:42 +0800

    progress

--- all.js
+++ all.js
@@ -1,3 +1,8 @@
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume pause"
+  });
+
 //基隆簡介效果
 let introductionTweens = document.querySelectorAll(".introductionTween");
 
@@ -9,5 +14,20 @@ introductionTweens.forEach(function(introductionTween, index){
     introductionTween.addEventListener("mouseenter" , () => tweenRed.play());
     introductionTween.addEventListener("mouseleave" , () => tweenRed.reverse());
 })
+
+gsap.to(".introduction", {scrollTrigger:".introduction", opacity:1 ,duration:10})
+// gsap.to(".h1", {scrollTrigger:".h1", duration:2, y:200})
 //基隆在哪裡效果
 gsap.fromTo("#location-alllogo img", {opacity:0},{y:-250, opacity:1 ,stagger:{each:1}})
+
+
+
+gsap.to('progress', {
+  value: 100,
+  ease: 'none',
+  scrollTrigger: { 
+    trigger: "#trip-title",
+    scrub: 0.2,
+    // start: "center center"
+  }
+});
