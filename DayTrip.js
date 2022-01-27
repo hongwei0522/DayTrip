5be926aae3e054b3e2e54ee4d0e3eaf0aaf4c355
redtail <hongwei0522@gmail.com>
2022-01-27 15:15:27 +0800

    trip-content

---
+++ DayTrip.js
@@ -0,0 +1,108 @@
+let DayTripData = 
+[
+  {
+    direction: "東岸",
+    location: "仙洞巖(佛手洞)",
+    content: "佛手洞位於仙洞巖旁約二、三十公尺不遠處，由仙洞巖旁步道拾階前往，這裡也是一處天然的海蝕洞，日治時代被當成防空洞使用，又因過去洞內曾有蝙蝠棲息，在早期曾被稱為蝙蝠洞，多年前由基隆市政府重新規劃改善而成一人文景觀區，並鋪設有碎石步道，防止溼滑。",
+    photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpAonSvK2OOX8nNystFpuhCAEjybEMcO8zVg&usqp=CAU",
+    creatTime: "EA1-1"
+  },
+  {
+    direction: "東岸",
+    location:"基隆燈塔",
+    content: "西元1900年建造完成的基隆燈塔，位於基隆港西岸碼頭萬人堆鼻。原為磚砌圓塔，民國51年（西元1962年）改建為鋼筋混凝土圓塔。萬人堆鼻原是一岩石林立的海埔地，因周邊奇岩怪石林立，如眾多鼻頭堆疊羅列，因而得名，不過現今已改建為三十號碼頭。基隆燈塔早年還設有紅色光弧，以標示港外的險礁，頗為特殊，後因險礁遭炸除，紅色光弧也遭撤除。",
+    photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpAonSvK2OOX8nNystFpuhCAEjybEMcO8zVg&usqp=CAU",
+    creatTime:"EA1-2"
+  },
+  {
+    direction: "東岸",
+    location:"白米甕砲台",
+    content: "白米甕砲臺位在基隆港口西岸高地上，與東岸的和平島互為犄角，共扼基隆港門，形勢非常險要，砲臺可能早在300多年前的西班牙及後來的荷蘭佔據時代就已設置，因此有「荷蘭城砲臺」之稱，清法戰爭時，法軍曾佔領過，今日之形貌為日治時代所改建而成的。",
+    photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpAonSvK2OOX8nNystFpuhCAEjybEMcO8zVg&usqp=CAU",
+    creatTime:"EA1-3"
+  },
+  {
+    direction: "東岸",
+    location:"外木山濱海風景區",
+    content: "外木山海岸為基隆市轄區僅存最長的天然海岸。自外木山漁港至澳底通村全長約5公里的海岸公路。一側為開闊的海岸景觀，一側為高聳的單面山懸崖，是極富壯麗景色的海岸線。在澳底漁村的沙灘是僅存的沙岸地型，小而美麗，夏日時成為民眾戲水游泳的好去處。夏季的日出是海岸不可多得的美景，坐在岸邊聆聽濤聲，欣賞彩霞萬丈之景，令人心曠神怡。海岸礁岩長年經海水沖蝕，雕塑出奇特的造形，值得民眾深入觀賞體會。",
+    photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpAonSvK2OOX8nNystFpuhCAEjybEMcO8zVg&usqp=CAU",
+    creatTime:"EA1-4"
+  },
+  {
+    direction: "東岸",
+    location:"情人湖公園",
+    content: "情人湖原名「五叉碑」，後來因救國團基隆團委員會經常在此辦活動，予以命名為情人湖。園區內有環湖步道、環山步道、情人吊橋、涼亭、觀景平台等設施，步道兩旁林木蓊鬱，而各種植物則展現四季風貌。漫步其中，在享受芬多精森林浴的同時，沿途並可欣賞湖光山色，相當詩情畫意。情人湖公園的環山步道可登上面海的山嶺老鷹岩，從稜線的步道上觀看大海，不僅可以見到岸邊的大武崙澳底漁港，亦可遠眺基隆嶼及野柳海岬，令人心胸頓時開朗舒暢。",
+    photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpAonSvK2OOX8nNystFpuhCAEjybEMcO8zVg&usqp=CAU",
+    creatTime:"EA1-5"
+  },
+  {
+    direction: "西岸",
+    location:"正濱漁港",
+    content: "",
+    photoUrl:"",
+    creatTime:"WE1-1"
+  },
+  {
+    direction: "西岸",
+    location:"和平島公園",
+    content: "",
+    photoUrl:"",
+    creatTime:"WE1-2"
+  },
+  { 
+    direction: "西岸",
+    location:"海科館",
+    content: "",
+    photoUrl:"",
+    creatTime:"WE1-3"
+  },
+  {
+    direction: "西岸",
+    location:"潮境公園",
+    content: "",
+    photoUrl:"",
+    creatTime:"WE1-4"
+  },
+  {
+    direction: "西岸",
+    location:"深奧鐵道自行車",
+    content: "",
+    photoUrl:"",
+    creatTime:"WE1-5"
+  },
+  {
+    direction: "市區",
+    location:"虎仔山",
+    content: "",
+    photoUrl:"",
+    creatTime:"CT1-1"
+  },
+  {
+    direction: "市區",
+    location:"獅球嶺砲台",
+    content: "",
+    photoUrl:"",
+    creatTime:"CT1-2"
+  },
+  {
+    direction: "市區",
+    location:"中正公園",
+    content: "",
+    photoUrl:"",
+    creatTime:"CT1-3"
+  },
+  {
+    direction: "市區",
+    location:"海門天險",
+    content: "",
+    photoUrl:"",
+    creatTime:"CT1-4"
+  },
+  {
+    direction: "市區",
+    location:"佛光洞",
+    content: "",
+    photoUrl:"",
+    creatTime:"CT1-5"
+  }
+]
--- all.js
+++ all.js
@@ -15,19 +15,77 @@ introductionTweens.forEach(function(introductionTween, index){
     introductionTween.addEventListener("mouseleave" , () => tweenRed.reverse());
 })
 
-gsap.to(".introduction", {scrollTrigger:".introduction", opacity:1 ,duration:10})
-// gsap.to(".h1", {scrollTrigger:".h1", duration:2, y:200})
+gsap.to(".introduction", {
+  scrollTrigger: {
+    trigger:".introduction", 
+    // markers:true, 
+    start: "top 20%",
+    end: "bottom 20%",
+    // scrub:true,
+    toggleActions: "restart pause reverse pause"}, 
+  opacity:1 ,duration:3})
+  
 //基隆在哪裡效果
-gsap.fromTo("#location-alllogo img", {opacity:0},{y:-250, opacity:1 ,stagger:{each:1}})
+gsap.to("#location-alllogo img", {
+  scrollTrigger: {
+    trigger:".location",
+    // markers:true,
+    start: "top center",
+    end: "center center",
+    scrub:true,
+    toggleActions: "restart pause reverse pause"},
+  y:-250, opacity:1 ,duration:3, stagger:{each:1}})
 
+gsap.to(".trip-progress", {
+  scrollTrigger: {
+    trigger: ".trip",
+    markers:true,
+    start: "top center",
+    end: "80% center",
+    scrub: true,
+  },
+  scaleY: 3,
+  transformOrigin: "top", 
+  ease: "none"
+})
 
+gsap.to(".trip-pin", {
+  scrollTrigger:{
+  trigger:".trip-pin",
+  start: "top 10%",
+  end: "+=930 10%",
+  // markers:true,
+  pin:true,
+},
+})
 
-gsap.to('progress', {
-  value: 100,
-  ease: 'none',
-  scrollTrigger: { 
-    trigger: "#trip-title",
-    scrub: 0.2,
-    // start: "center center"
+//一日遊
+let tripEast = document.getElementById("tripEast");
+
+model = function(dom, className, id, src, append, text, func){
+  let newElement = document.createElement(dom);
+  newElement.className = className;
+  newElement.id = id;
+  newElement.src = src;
+  newElement.textContent = text;
+  document.getElementById(append).appendChild(newElement);
+  newElement.onclick = func;
+}
+
+for(let i = 0; i <DayTripData.length; i++){
+  if(DayTripData[i].direction == "東岸"){
+    tripEastDiv(i)
   }
-});
+}
+
+function tripEastDiv(i){
+  model("div", "", DayTripData[i].creatTime, "", "tripEast", "", "");
+  model("img", "trip-location", "", DayTripData[i].photoUrl, DayTripData[i].creatTime, "", "");
+  model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].location, "");
+  model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].content, "");
+  // model("div", "trip-button", "tripBtnDiv", "", DayTripData[i].creatTime, "", "");
+  model("button", "trip-button-prev", "prevBtn", "", DayTripData[i].creatTime,"上一個景點", "");
+  model("button", "trip-button-next", "nextBtn", "", DayTripData[i].creatTime,"下一個景點", "");
+}
+
+//地圖
