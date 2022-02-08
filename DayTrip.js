ab1da8fee620f6be6fd3f2e227c49a58726a8c33
redtail <hongwei0522@gmail.com>
2022-02-08 14:57:22 +0800

    introduction-picture

--- DayTrip.js
+++ DayTrip.js
@@ -39,70 +39,70 @@ let DayTripData =
     direction: "西岸",
     location:"正濱漁港",
     content: "正濱漁港位於和平島南方，是歷史悠久的老漁港，早在西元1934年由日本人創造，日治時期為台灣第一大港，曾為繁榮極盛的漁貨商港，是早期帶動基隆漁港興盛的主要功臣，台灣北部重要的遠洋漁業基地，而在漁船噸位逐漸增加之後，漁港水位不堪負荷。現在雖然沒落，但仍保有樸實風貌，基隆市政府積極開發成為以假日休閒懷舊為主的漁碼頭，期待榮景將再現於此。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E6%AD%A3%E6%BF%B1%E6%BC%81%E6%B8%AF.jpg?raw=true",
     creatTime:"WE1-1"
   },
   {
     direction: "西岸",
     location:"和平島公園",
     content: "和平島公園位於基隆港港口東側，島上奇岩異石林立，海岸奇特的海蝕地形景觀，如海蝕平臺、豆腐岩、海蝕溝、海蝕崖、風化窗、萬人堆、千疊敷、海蝕洞、蕈狀岩等。本區雖長年受海風吹襲，但藻生植物（如紫菜、髮菜、鹿角菜、筊白菜、鵝菜、石花菜、紅毛苔、青菜等），魚生物（如黑毛、白毛、白帶魚、象魚、青魚、黃雞、龍蝦等），均隨季節的不同，而遨遊於公園周圍。公園以最少的人工設施來減少對自然破壞，以維繫自然生態的心來建設這美麗的自然寶藏，是北臺灣遊憩攬勝及自然教學的自然教室。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E5%92%8C%E5%B9%B3%E5%B3%B6%E5%85%AC%E5%9C%92.jpg?raw=true",
     creatTime:"WE1-2"
   },
   { 
     direction: "西岸",
     location:"海科館",
     content: "國立海洋科技博物館（簡稱海科館、海博館），位在基隆市東側的八斗子地區，西鄰八斗子、碧砂漁港、東接東北角海岸風景特定區，有台2線(濱海公路)及台鐵鐵路深澳支線經過，在1997年開始籌備，2013年甫完成所有工程之博物館，是除了屏東車城的海生館外，另一座以海洋為主題，闡述海洋和人類生活的交互關係；設立宗旨為永續發展海洋，推廣終身學習海洋知識和支援學校海洋教育之場所；藉此宣導海洋保育、生態知識，進而提升民眾對海洋、環保的尊重與實踐的人文素養，成為海洋教育觀光的重要調節點。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E6%B5%B7%E7%A7%91%E9%A4%A8.jpg?raw=true",
     creatTime:"WE1-3"
   },
   {
     direction: "西岸",
     location:"潮境公園",
     content: "「潮境」指的是寒、暖兩潮流的混合區，特別指西太平洋的黑潮與親潮的海水混合處，此區浮游生物孳生，能吸引洄游魚類聚集、成為漁場，也就是魚群較易匯集的地方，夏季時附近海域白帶魚、軟絲「大咬」，總是吸引不少釣客。潮境公園園區原本為一處垃圾集放地，後來經海洋科技博物館籌備處規劃改建後，成為一處看海賞景的最佳去處，而以「潮境」為名的這個公園，因為位於漁港及漁村旁，便希望以這個名稱為地方帶來源源不絕的蘊藏與豐收。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E6%BD%AE%E5%A2%83%E5%85%AC%E5%9C%92.jpg?raw=true",
     creatTime:"WE1-4"
   },
   {
     direction: "西岸",
     location:"深奧鐵道自行車",
     content: "鐵道自行車位於深澳支線-八斗子火車站與深澳火車站之間，早期深澳線在日治時期是一條軌距762公厘的輕便鐵路，也就是俗稱的「五分仔車」，主要是用於載運糖、鹽及煤至港口轉運出海，曾為風光一時之支線，與平溪線並駕齊驅，甚至有過之而無不及。但隨著礦源脈盡、煤礦減產，全線最後仍於1989年停止辦理客、貨運，僅保留瑞芳站至深澳火力發電廠之區間，提供運煤列車行駛，但因2007年發電廠停用，運煤需求消失，連帶深澳支線的歷史，因為產業而起，也因產業的消失而停駛，直至2014年因應海科館啟用，深澳支線也隨著復駛，八斗子車站也於2016年重新啟用。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E6%B7%B1%E5%A5%A7%E9%90%B5%E9%81%93%E8%87%AA%E8%A1%8C%E8%BB%8A.jpg?raw=true",
     creatTime:"WE1-5"
   },
   {
     direction: "市區",
     location:"虎仔山",
     content: "來到基隆，經常會被山頭上大大的「KEELUNG」造型的英文字母所吸引，那就是虎仔山所在的位置，而這個設計也讓虎仔山成為基隆的新興地標，這是在民國95年時由基隆市政府規劃興建的，經一再的修改及多方的努力之下，完成了此看版，衍然成為基隆市特有的休閒景觀，模仿好萊塢的設計，相當有創意，也讓許多基隆市民對這個設計引以為傲，外地遊客也備感新奇，開車沿著中山二路加油站旁的小路上行，經過垂直而上的車塔隧道後，即可抵虎仔山。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E8%99%8E%E4%BB%94%E5%B1%B1.jpg?raw=true",
     creatTime:"CT1-1"
   },
   {
     direction: "市區",
     location:"獅球嶺砲台",
     content: "獅球嶺砲台位於基隆市仁愛區的獅球嶺頂上，北基高速公路隧道上，連貫基隆山、虎山而呈凹形，地勢居高臨下，敵軍不易侵入，成為基隆港的重要防線，在此可俯瞰整個基隆市區。砲台高150公尺，始建於1884年(清光緒十年)，在許多大大小小戰役中，皆發揮了禦敵的功能，直到日治時期，砲台被荒廢，漸失軍事功能。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E7%8D%85%E7%90%83%E5%B6%BA%E7%A0%B2%E5%8F%B0.jpg?raw=true",
     creatTime:"CT1-2"
   },
   {
     direction: "市區",
     location:"中正公園",
     content: "基隆中正公園在基隆港東側山丘上，依山建築，草木扶疏，是市民平日休閒的最佳去處。中正公園的正門位於信二路上，可拾階而上，或由側邊的斜坡步行而上。公園本身共分成三層，第一層有涼亭、棋藝中心以及運動休閒設施，假日常吸引大批人潮來此遊玩。第二層則為「主普壇」的所在地。每年基隆中元普渡的祭典就是在這裡舉行的，屆時人山人海，場面十分壯觀。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E4%B8%AD%E6%AD%A3%E5%85%AC%E5%9C%92.jpg?raw=true",
     creatTime:"CT1-3"
   },
   {
     direction: "市區",
     location:"海門天險",
     content: "於西元1840年清英鴉片戰爭時，為防止英軍入侵而建，西元1884年清法戰爭毀損後劉銘傳（臺灣首任巡撫）又重建，因位處於二沙灣一帶的山路上，因此有「二沙灣砲臺」的別稱。海門天險因地制宜，東、北二個砲臺分守外海及內海口門，建於較高的臨海山頭，低處背海面則為營區。砲臺區內可看到大砲、彈藥庫等；而營盤區則遺有數段牆垣和刻有「海門天險」四個大字的營門。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E6%B5%B7%E9%96%80%E5%A4%A9%E9%9A%AA.jpg?raw=true",
     creatTime:"CT1-4"
   },
   {
     direction: "市區",
     location:"佛光洞",
     content: "佛光洞位於紅淡山步道內，原本是個蝙蝠洞，洞內極為寬敞高大，洞頂一片巨岩斜平往下，原本是小溪流經的伏流岩洞，目前洞內供奉觀音像，洞口有一尊巨大的白色彌勒佛，為台灣最大山洞寺廟。洞中可容納近一千人，二次大戰期間作為基隆市民的防空洞，推測因當時洞內設架舖蓆，蝙蝠遷徙無蹤。",
-    photoUrl:"",
+    photoUrl:"https://github.com/hongwei0522/DayTrip/blob/master/picture/%E4%BD%9B%E5%85%89%E6%B4%9E.jpg?raw=true",
     creatTime:"CT1-5"
   }
 ]
--- all.js
+++ all.js
@@ -36,29 +36,7 @@ gsap.to("#location-alllogo img", {
     toggleActions: "restart pause reverse pause"},
   y:-250, opacity:1 ,duration:3, stagger:{each:1}})
 
-  //標籤效果
-gsap.to(".trip-progress", {
-  scrollTrigger: {
-    trigger: ".trip",
-    // markers:true,
-    start: "top center",
-    end: "80% center",
-    scrub: true,
-  },
-  scaleY: 3,
-  transformOrigin: "top", 
-  ease: "none"
-})
 
-gsap.to(".trip-pin", {
-  scrollTrigger:{
-  trigger:".trip-pin",
-  start: "top 10%",
-  end: "+=930 10%",
-  // markers:true,
-  pin:true,
-},
-})
 
 
 
@@ -78,7 +56,10 @@ model = function(dom, className, id, src, append, text, func){
 for(let i = 0; i <DayTripData.length; i++){
   if(DayTripData[i].direction == "東岸"){
     tripEastDiv(i)
-    tripMaps(i)
+  }else if(DayTripData[i].direction == "西岸"){
+    tripWestDiv(i)
+  }else if(DayTripData[i].direction == "市區"){
+    tripCenterDiv(i)
   }
 }
 
@@ -86,29 +67,110 @@ function tripEastDiv(i){
   model("div", "tripEastDiv", DayTripData[i].creatTime, "", "tripEast", "", "");
   model("img", "trip-location", "", DayTripData[i].photoUrl, DayTripData[i].creatTime, "", "");
   model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].location, "");
-  model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].content, "");
-  // model("div", "trip-button", "tripBtnDiv", "", DayTripData[i].creatTime, "", "");
-  model("button", "trip-button-prev", "prevBtn", "", DayTripData[i].creatTime,"上一個景點", "");
-  model("button", "trip-button-next", "nextBtn", "", DayTripData[i].creatTime,"下一個景點", "");
+  model("p", "trip-center-word-left", "", "", DayTripData[i].creatTime, DayTripData[i].content, "");
+  model("div", "trip-button", "tripBtnDiv"+[i], "", DayTripData[i].creatTime, "", "");
+  model("button", "trip-button-prev", "prevBtn", "", "tripBtnDiv"+[i],"上一個景點", "");
+  model("button", "trip-button-next", "nextBtn", "", "tripBtnDiv"+[i],"下一個景點", "");
+}
+function tripWestDiv(i){
+  model("div", "tripWestDiv", DayTripData[i].creatTime, "", "tripWest", "", "");
+  model("img", "trip-location", "", DayTripData[i].photoUrl, DayTripData[i].creatTime, "", "");
+  model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].location, "");
+  model("p", "trip-center-word-left", "", "", DayTripData[i].creatTime, DayTripData[i].content, "");
+  model("div", "trip-button", "tripBtnDiv"+[i], "", DayTripData[i].creatTime, "", "");
+  model("button", "trip-button-prev", "prevBtn", "", "tripBtnDiv"+[i],"上一個景點", "");
+  model("button", "trip-button-next", "nextBtn", "", "tripBtnDiv"+[i],"下一個景點", "");
+}
+function tripCenterDiv(i){
+  model("div", "tripCenterDiv", DayTripData[i].creatTime, "", "tripCenter", "", "");
+  model("img", "trip-location", "", DayTripData[i].photoUrl, DayTripData[i].creatTime, "", "");
+  model("p", "trip-center-word", "", "", DayTripData[i].creatTime, DayTripData[i].location, "");
+  model("p", "trip-center-word-left", "", "", DayTripData[i].creatTime, DayTripData[i].content, "");
+  model("div", "trip-button", "tripBtnDiv"+[i], "", DayTripData[i].creatTime, "", "");
+  model("button", "trip-button-prev", "prevBtn", "", "tripBtnDiv"+[i],"上一個景點", "");
+  model("button", "trip-button-next", "nextBtn", "", "tripBtnDiv"+[i],"下一個景點", "");
 }
 
-//地圖
-function tripMaps(i){
-  let tripMap = document.getElementById("tripMap");
-  console.log(tripMap.clientHeight)
-  let a = document.getElementById(DayTripData[i].creatTime)
-  a.clientHeight = tripMap.clientHeight
-  console.log(a.clientHeight)
 
-  // document.getElementById(DayTripData[i].clientHeight) = tripMap.clientHeight
-}
+let tripEastDivHeight = document.querySelector('.tripEastDiv').clientHeight;
+let tripContentHeight = tripEastDivHeight*4;
+console.log(tripContentHeight)
+
+//標籤效果
+//東岸
+gsap.to(".trip-progressEast", {
+  scrollTrigger: {
+    trigger: ".trip",
+    // markers:true,
+    start: "top center",
+    end: "80% center",
+    scrub: true,
+  },
+  scaleY: 5.5,
+  transformOrigin: "top", 
+  ease: "none"
+})
+gsap.to(".trip-pin-east", {
+  scrollTrigger:{
+  trigger:".trip-pin-east",
+  start: "top 10%",
+  end: `${tripContentHeight} top`,
+  // markers:true,
+  pin:true,
+},
+})
+//西岸
+gsap.to(".trip-progressWest", {
+  scrollTrigger: {
+    trigger: ".tripWest",
+    // markers:true,
+    start: "top center",
+    end: "80% center",
+    scrub: true,
+  },
+  scaleY: 5.5,
+  transformOrigin: "top", 
+  ease: "none"
+})
+gsap.to(".trip-pin-west", {
+  scrollTrigger:{
+  trigger:".trip-pin-west",
+  start: "top 10%",
+  end: `${tripContentHeight} top`,
+  // markers:true,
+  pin:true,
+},
+})
+//市區
+gsap.to(".trip-progressCenter", {
+  scrollTrigger: {
+    trigger: ".tripCenter",
+    // markers:true,
+    start: "top center",
+    end: "80% center",
+    scrub: true,
+  },
+  scaleY: 5.5,
+  transformOrigin: "top", 
+  ease: "none"
+})
+gsap.to(".trip-pin-center", {
+  scrollTrigger:{
+  trigger:".trip-pin-center",
+  start: "top 10%",
+  end: `${tripContentHeight} top`,
+  // markers:true,
+  pin:true,
+},
+})
 
 //地圖效果
+//東岸
 gsap.to(".trip-map", {
   scrollTrigger:{
   trigger:".trip-map",
   start: "top top",
-  end: "+=930 top",
+  end: `${tripContentHeight}  top`,
   // markers:true,
   pin:true,
 },
@@ -118,7 +180,7 @@ gsap.to(".tripStation", {
   scrollTrigger:{
   trigger:".tripStation",
   start: "top top",
-  end: "+=930 top",
+  end: `${tripContentHeight} top`,
   // markers:true,
   toggleActions: "play reset play reset"
 },
@@ -169,3 +231,135 @@ gsap.to(".EA1-5", {
 },
 opacity:1,duration:3})
 
+
+//西岸
+gsap.to("#tripWestMap", {
+  scrollTrigger:{
+  trigger:"#tripWestMap",
+  start: "top top",
+  end: `${tripContentHeight}  top`,
+  // markers:true,
+  pin:true,
+},
+})
+
+gsap.to(".tripStationWest", {
+  scrollTrigger:{
+  trigger:".tripStationWest",
+  start: "top top",
+  end: `${tripContentHeight} top`,
+  // markers:true,
+  toggleActions: "play reset play reset"
+},
+opacity:0})
+
+gsap.to(".WE1-1", {
+  scrollTrigger:{
+  trigger:"#WE1-1",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".WE1-2", {
+  scrollTrigger:{
+  trigger:"#WE1-2",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".WE1-3", {
+  scrollTrigger:{
+  trigger:"#WE1-3",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".WE1-4", {
+  scrollTrigger:{
+  trigger:"#WE1-4",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".WE1-5", {
+  scrollTrigger:{
+  trigger:"#WE1-5",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+//市區
+gsap.to("#tripCenterMap", {
+  scrollTrigger:{
+  trigger:"#tripCenterMap",
+  start: "top top",
+  end: `${tripContentHeight}  top`,
+  // markers:true,
+  pin:true,
+},
+})
+
+gsap.to(".tripStationCenter", {
+  scrollTrigger:{
+  trigger:".tripStationCenter",
+  start: "top top",
+  end: `${tripContentHeight} top`,
+  // markers:true,
+  toggleActions: "play reset play reset"
+},
+opacity:0})
+
+gsap.to(".CT1-1", {
+  scrollTrigger:{
+  trigger:"#CT1-1",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".CT1-2", {
+  scrollTrigger:{
+  trigger:"#CT1-2",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".CT1-3", {
+  scrollTrigger:{
+  trigger:"#CT1-3",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".CT1-4", {
+  scrollTrigger:{
+  trigger:"#CT1-4",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
+
+gsap.to(".CT1-5", {
+  scrollTrigger:{
+  trigger:"#CT1-5",
+  start: "top top",
+  end: "bottom top",
+  toggleActions: "play reset play reset"
+},
+opacity:1,duration:3})
